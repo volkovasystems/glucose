67eb882b1266a9073812c7038fc55ea2417c4099
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 16:34:41 +0800

    Updated test.

--- glucose-test.js
+++ glucose-test.js
@@ -2,21 +2,22 @@ const assert = require( "assert" )
 const glucose = require( "./glucose.js" );
 
 assert.equal( JSON.stringify( glucose( { "hello": "world" } ) ),
-JSON.stringify( {
-	"factor": [ ],
-	"identity": { },
-	"setting": { },
-	"query": { },
-	"pagination": { },
-	"data": { },
-	"list": [ ],
-	"element": { },
-	"array": { },
-	"scope": [ ],
-	"permission": [ ],
-	"hello": "world"
-} ) );
+	JSON.stringify( {
+		"factor": [ ],
+		"identity": { },
+		"setting": { },
+		"query": { },
+		"pagination": { },
+		"data": { },
+		"list": [ ],
+		"element": { },
+		"array": { },
+		"scope": [ ],
+		"permission": [ ],
+		"hello": "world"
+	} ),
+	"{'factor':[],'identity':{},'setting':{},'query':{},'pagination':{},'data':{},'list':[],'element':{},'array':{},'scope':[],'permission':[],'hello':'world'}" );
 
-assert.equal( typeof glucose( { "hello": "world" } ) == "object", true, "should return true" );
+assert.equal( typeof glucose( { "hello": "world" } ) == "object", true, "should be of object data type" );
 
 console.log( "ok" );
